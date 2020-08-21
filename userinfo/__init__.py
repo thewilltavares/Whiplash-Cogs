5f09e85ecb8dc6f867e9fab756e8f9e89e4b2a64
thewilltavares <57693889+thewilltavares@users.noreply.github.com>
2020-08-21 14:12:54 -0400

    Update __init__.py

--- userinfo/__init__.py
+++ userinfo/__init__.py
@@ -5,8 +5,7 @@ __red_end_user_data_statement__ = (
     "This cog does not persistently store data or metadata about users."
 )
 
-
-async def setup(bot):
-    cog = UserInfo
-    await cog.initialize()
+async def setup(bot: Red):
+    cog = UserInfo(bot)
     bot.add_cog(cog)
+    await cog.initialize()
